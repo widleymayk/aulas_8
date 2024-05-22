d02db5ff5675e8a1d940fe58c6e91dc8d08a8702
widleymayk <widleywmss@gmail.com>
2024-05-22 14:28:22 -0300

    Add files via upload

---
+++ primeiro-exercicioPOO.py
@@ -0,0 +1,60 @@
+class Pessoas:
+
+
+    def __init__(self,nome,idade,peso, altura):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+        self.altura = altura
+    def envelhecer(self):
+        self.idade += 1 
+        if self.idade < 21:
+            self.crescer += (0.05) 
+        return self.idade
+    def engordar(self, kilos):
+        self.peso += kilos
+        return self.peso
+    def emagrecer(self,kilos_perdidos):
+        self.peso -= kilos_perdidos
+        return self.peso
+    def crescer(self, anos ):
+        if self.idade < 21:
+            self.altura += (0.05) * anos
+        return self.altura
+    def __init__(self, nome, idade, peso, altura):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+        self.altura = altura
+
+    def envelhecer(self, anos):
+        self.anos = anos
+        self.idade += anos
+        print('\nA pessoa envelheceu ',anos,' anos\nA idade atual é ',self.idade,' anos')
+
+    def engordar(self, pesagem):
+        self.peso += pesagem
+        print('\nA pessoa engordou ',pesagem,' kg\nO peso atual é',round(self.peso),'kg')
+
+    def emagrecer(self, pesagem):
+        self.peso -= pesagem
+        print('\nA pessoa emagreceu ',pesagem,' kg\nO peso atual é',round(self.peso),'kg')
+
+    def crescer(self, anos):
+        self.idade += 1
+        if self.idade < 21:
+            crescimento = 0.05 * anos
+            self.altura += crescimento
+            print('\nA pessoa cresceu ',crescimento,'m\nA altura atual é',self.altura,'m')
+        else:
+            print('\nA pessoa não cresceu')
+
+
+
+pessoa_1 = Pessoas ('Mayk', 15, 76.10, 1.75)
+print (pessoa_1.nome)
+pessoa_1.envelhecer(5)
+pessoa_1.engordar(10.1)
+pessoa_1.emagrecer(5)
+pessoa_1.crescer(4)
+
